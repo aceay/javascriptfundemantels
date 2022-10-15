8d10780dfa601a7b5598afc83d875c2f6ac68170
aceay <aceayesma@gmail.com>
2022-10-15 16:42:50 +0300

    javascriptfundementals

---
+++ literal.js
@@ -0,0 +1,21 @@
+console.log(`i counted ${10+23} cows`);
+console.log(`i counted ${1,10}`);
+// console.log(`i laughed ${hahaha}`);
+
+let firstName ='ESTHER';
+let lastName = 'macharia';
+// let fullName =firstName + ' ' + lastName;
+// console.log(fullName);
+let fullName =`${firstName} ${lastName}`;
+console.log(fullName);
+let totalone = 500;
+let totaltwo=800;
+let total = `ksh${totalone + totaltwo}`;
+console.log(total);
+// total.includes (total);
+// console.log(total);
+let item = `cucamber`;
+let price = 1.99;
+let quantity = 4;
+let exact=`you bought ${quantity} ${item} total price $${price * quantity}`;
+console.log(exact)
---
+++ strings.js
@@ -0,0 +1,19 @@
+let annoyinLaugh = "teehee so funny! teehee!";
+console.log(annoyinLaugh);
+annoyinLaugh.replaceAll("teehee","haha");
+console.log(annoyinLaugh.replace("teehee","haha"));
+annoyinLaugh.replace("teehee,hoho");
+console.log(annoyinLaugh.replaceAll("teehee","hoho"));
+console.log();
+
+const age = "5" + "4";
+console.log(age);
+
+"pecan pie"[7];
+console.log("pecan pie"[7]);
+console.log();"PUP"[3];
+console.log('hello\nworld');
+console.log("hello\n\'world\'");
+console.log("hello\\\nworld");
+console.log("\\hello\\\n\'world\'");
+console.log("hello\rworld");
